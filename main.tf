a6dbc67536b7f8ee96adcb32e8960d9b8696b1be
Matt Ashby <matt.ashby@theinstillery.com>
2020-03-05 15:24:04 +1300

    adding additional modules

---
+++ main.tf
@@ -0,0 +1,32 @@
+module "level0" {
+  source = "../tf-mod-levelzero"
+
+  fw_rg_enabled = var.level0_fw_rg_enabled
+  la_rg_enabled = var.level0_la_rg_enabled
+
+}
+
+# firewall vnet
+resource "azurerm_virtual_network" "firewall" {
+  count = var.fw_vnet_enabled == true ? length(keys(var.fw_vnet_config.location)) : 0
+  name  = element(values(var.fw_vnet_config.vnet_name), count.index)
+  # location            = azurerm_resource_group.firewall.*.location[count.index]
+  # resource_group_name = azurerm_resource_group.firewall.*.name[count.index]
+  location            = module.level0.level0_firewall_location.*.location[count.index]
+  resource_group_name = module.level0.level0_firewall_rg_name.*.name[count.index]
+  address_space       = element(values(var.fw_vnet_config.address_space), count.index)
+  dns_servers         = element(values(var.fw_vnet_config.dns_servers), count.index)
+  tags                = element(values(var.fw_vnet_config.tags), count.index)
+}
+
+# Subnet
+resource "azurerm_subnet" "default" {
+  count = var.fw_vnet_enabled == true ? length(keys(var.fw_vnet_config.location)) : 0
+  name  = element(values(var.vnet_config.subnet_name), count.index)
+  # resource_group_name  = azurerm_resource_group.firewall.*.name[count.index]
+  virtual_network_name = azurerm_virtual_network.firewall.*.name[count.index]
+  resource_group_name  = module.level0.level0_firewall_rg_name.*.name[count.index]
+  # network_security_group_id = azurerm_network_security_group.default.*.id[count.index]
+  # route_table_id            = azurerm_route_table.default.*.id[count.index]
+  address_prefix = element(values(var.fw_vnet_config.subnet_prefix), count.index)
+}
