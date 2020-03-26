abf79277ab8316d3410b3781209ac7fccfa14393
Matt Ashby <matt@ashbys.nz>
2020-03-26 22:29:50 +1300

    added Recovery Services Vault addeed Key Vault

--- variables.tf
+++ variables.tf
@@ -1,3 +1,30 @@
 variable "level0_NSG" {}
 variable "level0_subnet" {}
 variable "level0_rt" {}
+variable "level0_rg" {}
+
+variable "tenant_id" {}
+variable "object_id" {}
+
+variable "tags" {
+  default     = ""
+  description = "(Required) tags for the deployment"
+}
+
+variable "rsv_suffix" {
+  description = "(Optional) You can use a suffix to add to the list of Recovery Service Vaults you want to create"
+  type        = string
+}
+
+variable "rsv_object" {
+  description = "(Required) configuration object describing the Recovery Service Vault configuration"
+}
+
+variable "key_vault_suffix" {
+  description = "(Optional) You can use a suffix to add to the list of Key Vaults you want to create"
+  type        = string
+}
+
+variable "key_vault_object" {
+  description = "(Required) configuration object describing the Key Vault configuration"
+}
